907e8ebafdcaec4cf473e8d9ef6b62e28ebc2dc9
Matheus Migge <matheusmigge@gmail.com>
2025-03-28 15:11:24 -0300

    small-card component

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-small-card',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
+  @Input()
+  id: string = "0";
   @Input()
   postCategory: string = "";
   @Input()
