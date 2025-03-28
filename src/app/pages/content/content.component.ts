af16bb9f189987544f61f62add8b57a32c305cae
Matheus Migge <matheusmigge@gmail.com>
2025-03-28 15:11:51 -0300

    Update content component styles and logic

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -8,16 +10,43 @@ import { Component, Input } from '@angular/core';
 })
 export class ContentComponent {
   @Input()
+  id: string | null = "0";
+  @Input()
   postCategory: string = "";
   @Input()
   postTitle: string = "";
   @Input()
-  postSubtitle: string = "";
+  postHeadline: string = "";
   @Input()
   postCoverURL: string = "";
   @Input()
   postAuthorAvatarURL: string = "";
   @Input()
   postAuthor: string = "";
+  @Input()
+  postText: string = "";
 
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+     this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.postTitle= result.postTitle
+    this.postCategory= result.postCategory
+    this.postHeadline= result.postHeadline
+    this.postCoverURL= result.postCoverURL
+    this.postAuthorAvatarURL= result.postAuthorAvatarURL
+    this.postAuthor= result.postAuthor
+    this.postText= result.postText;
+  }
 }
